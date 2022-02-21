19bff99dceddc3c37799a0b0ee5a6eeef59297a9
Sergey Khavrolev <sergey.khavrolev@introduct.tech>
2022-02-21 16:37:58 +0300

    delete cross references to models

--- src/spots/spots.module.ts
+++ src/spots/spots.module.ts
@@ -3,12 +3,11 @@ import { SpotsService } from './spots.service';
 import { SpotsController } from './spots.controller';
 import { Spot } from '../models/spots.model';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Transport } from 'src/models/transports.model';
 
 @Module({
   controllers: [SpotsController],
   providers: [SpotsService],
-  imports: [SequelizeModule.forFeature([Spot, Transport])],
+  imports: [SequelizeModule.forFeature([Spot])],
   exports: [SpotsService],
 })
 export class SpotsModule {}
--- src/transports/transports.module.ts
+++ src/transports/transports.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Spot } from 'src/models/spots.model';
 import { SpotsModule } from 'src/spots/spots.module';
 import { TransportsController } from './transports.controller';
 import { Transport } from '../models/transports.model';
@@ -9,7 +8,7 @@ import { TransportsService } from './transports.service';
 @Module({
   controllers: [TransportsController],
   providers: [TransportsService],
-  imports: [SequelizeModule.forFeature([Transport, Spot]), SpotsModule],
+  imports: [SequelizeModule.forFeature([Transport]), SpotsModule],
   exports: [TransportsService],
 })
 export class TransportsModule {}
