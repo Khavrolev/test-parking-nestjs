e5d00179828716bb6eced582d8968fa2c3b1049d
Sergey Khavrolev <sergey.khavrolev@introduct.tech>
2022-02-23 15:41:12 +0300

    add unit test for spots.controller

--- src/spots/spots.controller.spec.ts
+++ src/spots/spots.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { SpotType, SPOT_SIZE } from '../constants/spots.constants';
 import { SpotsController } from './spots.controller';
 import { SpotsService } from './spots.service';
 
@@ -6,26 +7,27 @@ describe('SpotsController', () => {
   let controller: SpotsController;
 
   const mockSpotsService = {
-    // createSpot: jest.fn((dto) => {
-    //   return {
-    //     id: 1,
-    //     size: SPOT_SIZE[dto.type],
-    //   };
-    // }),
+    createSpot: jest.fn((dto) => {
+      return {
+        id: 1,
+        size: SPOT_SIZE[dto.type],
+      };
+    }),
+    updateSpot: jest.fn().mockImplementation((dto) => {
+      return {
+        id: dto.id,
+        size: SPOT_SIZE[dto.type],
+      };
+    }),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SpotsController],
-      providers: [
-        {
-          provide: SpotsService,
-          useValue: {},
-        },
-      ],
+      providers: [SpotsService],
     })
-      // .overrideProvider(SpotsService)
-      // .useValue(mockSpotsService)
+      .overrideProvider(SpotsService)
+      .useValue(mockSpotsService)
       .compile();
 
     controller = module.get<SpotsController>(SpotsController);
@@ -35,10 +37,24 @@ describe('SpotsController', () => {
     expect(controller).toBeDefined();
   });
 
-  // it('should create spot', () => {
-  //   expect(controller.createSpot({ type: SpotType.medium })).toEqual({
-  //     id: expect.any(Number),
-  //     size: 2,
-  //   });
-  // });
+  it('should create spot', () => {
+    const dto = { type: SpotType.medium };
+    expect(controller.createSpot(dto)).toEqual({
+      id: expect.any(Number),
+      size: 2,
+    });
+
+    expect(mockSpotsService.createSpot).toHaveBeenCalledWith(dto);
+  });
+
+  it('should update spot', () => {
+    const dto = { id: 5, type: SpotType.medium };
+
+    expect(controller.updateSpot(dto)).toEqual({
+      id: 5,
+      size: 2,
+    });
+
+    expect(mockSpotsService.updateSpot).toHaveBeenCalledWith(dto);
+  });
 });
